32ba5af687a2af8fb0d1a0d98d8447ebde9ffb45
Vasyl <savytskyivasyl@gmail.com>
2015-05-16 16:08:40 +0300

    add stub controller

---
+++ src/main/resources/static/js/controllers/adminController.js
@@ -0,0 +1,4 @@
+angular.module('admin.controllers',['customDirectives', 'ui.bootstrap'])
+    .controller('adminController', function($scope, $rootScope, $modal, httpService, $controller) {
+
+    });
