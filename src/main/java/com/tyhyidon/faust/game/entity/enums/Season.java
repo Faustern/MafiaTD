84cb9ec40de2872b58fd8770508b9cf354a4717a
Vasyl <savytskyivasyl@gmail.com>
2017-07-19 15:55:23 +0300

    Update Season.java

--- src/main/java/com/tyhyidon/faust/game/entity/enums/Season.java
+++ src/main/java/com/tyhyidon/faust/game/entity/enums/Season.java
@@ -4,5 +4,5 @@ package com.tyhyidon.faust.game.entity.enums;
  * Created by vasylsavytskyi on 11.05.15.
  */
 public enum Season {
-    ALL, WINTER_13_14, SPRING_15, SUMMER_15, SPRING_16
+    ALL, WINTER_13_14, SPRING_15, SUMMER_15, SPRING_16, SUMMER_17
 }
